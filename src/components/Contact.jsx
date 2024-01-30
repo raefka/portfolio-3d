a9280075124e81f6638400b492a98bcea70f7278
raefka <raefkacem20@gmail.com>
2024-01-30 16:08:10 +0100

    second commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,105 @@
+import {useState , useRef} from 'react';
+import {motion} from 'framer-motion';
+import emailjs from'@emailjs/browser';
+
+import {styles} from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
+
+
+const Contact = () => {
+  const formRef = useRef();
+   const [form,setForm] = useState({
+    name:'',
+    emial:'',
+    message:'',
+   });
+
+   const [loading, setLoading] = useState(false);
+
+   const handleChange =(e) =>{
+
+   } ;
+
+   const handleSubmit =(e) =>{
+
+   }
+
+  return (
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div
+        variants={slideIn('left',"tween" ,0.2,1)}
+        className='flex-[0.75px] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form 
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col '>
+              <span className='text-white font-medium mb-4'>
+                 Your name :
+              </span>
+              <input
+                type='text'
+                name='name'
+                value={form.name}
+                onChange={handleChange}
+                placeholder="what's your name ?"
+                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium  '
+              />
+          </label>
+
+          <label className='flex flex-col '>
+              <span className='text-white font-medium mb-4'>
+                 Your Message :
+              </span>
+              <input
+                type='email'
+                name='email'
+                value={form.email}
+                onChange={handleChange}
+                placeholder="what's your email ?"
+                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium  '
+              />
+          </label>
+
+          <label className='flex flex-col '>
+              <span className='text-white font-medium mb-4'>
+                 Your Message :
+              </span>
+              <textarea
+                rows="7"
+                name='message'
+                value={form.message}
+                onChange={handleChange}
+                placeholder="what do you want to say ?"
+                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium  '
+              />
+          </label>
+
+          <button
+            type='submit'
+            className='bg-green-400 py-3 px-8 outline-none w-fit text-primary font-bold shadow-md shadow-primary rounded-lg'
+          >
+            {loading ? 'Sending ...' : 'Send'}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+         variants={slideIn('right',"tween" ,0.2,1)}
+         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
+
+    </div>
+  )
+}
+
+export default SectionWrapper(Contact , "contact") 
---
+++ src/constants/index.js
@@ -0,0 +1,221 @@
+import {
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    
+   
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Alone",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2022 - April 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "front Developer",
+      company_name: "Alone",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Jan 2024",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2023 - Jan 2024",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "Alone",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Nextjs",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/raefka/car-showcase",
+    },
+    {
+      name: "Travel page",
+      description:
+        "Web application that enables users to search for places that you can visit , and enjoy the breathtaking sights.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "TypeScript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Tailwind css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/raefka/travel-app",
+    },
+    {
+      name: "Event App",
+      description:
+        "A comprehensive event booking platform that allows users to book events , create  and enjoy learning  for popular courses.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/raefka/event-app",
+    },
+  ];
+  
+  export { services, technologies, experiences, projects };
